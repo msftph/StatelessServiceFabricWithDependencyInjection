ec93bcb66a473f5662f546c6041ac8aeba8e570d
Patrick Huber <65667413+msftph@users.noreply.github.com>
2025-04-29 11:26:32 -0400

    Add project files.

---
+++ StatelessServiceWithDI/Program.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Diagnostics;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace StatelessServiceWithDI
+{
+    public static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        public static void Main()
+        {
+            try
+            {
+                // The ServiceManifest.XML file defines one or more service type names.
+                // Registering a service maps a service type name to a .NET type.
+                // When Service Fabric creates an instance of this service type,
+                // an instance of the class is created in this host process.                
+                Task registerServiceAsyncTask = ServiceRuntime.RegisterServiceAsync("StatelessServiceWithDIType",
+                    context => {
+                        IHost host = CreateHost(context);
+                        return host.Services.GetRequiredService<StatelessServiceWithDI>();
+                    });
+                registerServiceAsyncTask.GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(StatelessServiceWithDI).Name);
+
+                // Prevents this host process from terminating so services keep running.
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+
+        public static IHost CreateHost(StatelessServiceContext context)
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {                    
+                    services.AddSingleton(context);
+                    services.AddSingleton<StatelessServiceWithDI>();
+                })
+                .Build();
+        }
+    }
+}
